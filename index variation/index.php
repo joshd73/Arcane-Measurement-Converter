2b8ff43657f964c659646ca3e3ebbc27c4b52bd4
Josh Dettloff <joshuaddettloff@students.abtech.edu>
2020-11-12 19:55:41 -0500

    Init commit

---
+++ index variation/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--	Author: 	Josh Dettloff
+		Date:	    11/11/2020
+		File:		index.php
+		Purpose:	Liquid conversions
+-->
+
+<html>
+<head>
+	<title>Archaic Conversions</title>
+	<link rel ="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<body>
+    <h1>Conversions</h1>
+    <button onclick="window.location.href='liquids.php';">
+      Liquids
+    </button>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Archaic Conversions</title>
+    <link href="styles.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+
+    <div id="main-content">
+
+      <h1>Conversions</h1>
+  
+      <ul id="measurement-types">
+        <li><a href="liquids.php">Liquids</a></li>
+      </ul>
+  
+    </div>
+  </body>
+</html>
